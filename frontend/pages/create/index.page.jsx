81c1b2e8daa2b67874197db176aa46b5ff60e666
Haiz14 <haiz.gigs@gmail.com>
2023-05-08 13:10:40 +0530

    Added view, and create routes

---
+++ frontend/pages/create/index.page.jsx
@@ -0,0 +1,9 @@
+export { Page }
+
+function Page() {
+	return (
+		<div>
+			<h1>index.page.jsx</h1>
+		</div>
+	)
+}
---
+++ frontend/pages/view/index.page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export { Page }
+
+function Page() {
+	return (
+		<div>
+			<h1>index.page.jsx</h1>
+		</div>
+	)
+}
