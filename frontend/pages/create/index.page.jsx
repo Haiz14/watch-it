5f449dcc4a14220ad3cb4736b6164ae738633775
Haiz14 <haiz.gigs@gmail.com>
2023-05-09 16:42:25 +0530

    ADDED: Peerjs js client

--- frontend/pages/create/index.page.jsx
+++ frontend/pages/create/index.page.jsx
@@ -1,13 +1,35 @@
-/** 
-  add Button start-stream and stop-stream
-*/
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
+import Peer from "peerjs";
 import SetupStream from "./SetupStream";
 
 export function Page() {
   const [stream, setStream] = useState(null);
   const [peerId, setPeerId] = useState(null);
+  const [peer, setPeer] = useState(null);
   const [streamOn, setStreamOn] = useState(false);
+  const [remotePeerId, setRemotePeerId] = useState("");
+
+  useEffect(() => {
+    if (stream) {
+      const newPeer = new Peer();
+      newPeer.on("open", (id) => {
+        console.log("Peer ID:", id);
+        setPeerId(id);
+      });
+
+      newPeer.on("call", (call) => {
+        call.answer(stream);
+      });
+
+      setPeer(newPeer);
+    }
+
+    return () => {
+      if (peer) {
+        peer.destroy();
+      }
+    };
+  }, [stream]);
 
   const handleStartStream = () => {
     setStreamOn(true);
@@ -17,12 +39,13 @@ export function Page() {
     setStreamOn(false);
   };
 
+
   return (
     <>
+      <p>Peer ID: {peerId}</p>
       <SetupStream setStream={setStream} streamOn={streamOn} />
       <button onClick={handleStartStream}>Start Stream</button>
       <button onClick={handleStopStream}>Stop Stream</button>
     </>
   );
 }
-
