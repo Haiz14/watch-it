5957eac378e94153a72554fa1d2adb3814497ed7
Haiz14 <haiz.gigs@gmail.com>
2023-05-09 16:15:28 +0530

    ADDED: Start stop stream button

--- frontend/pages/create/SetupStream.jsx
+++ frontend/pages/create/SetupStream.jsx
@@ -1,27 +1,30 @@
-/**
- * SetupStream component
- * receive setStream props
- * get video stream via navigator.mediaDevices.getUserMedia
- * set Stream to video element
-*/
-
+/*
+ * receives prop, streamOn: bool, which starts and stops stream
+ */
 import React, { useEffect, useRef } from "react";
 
-export default function SetupStream({ setStream }) {
+export default function SetupStream({ setStream, streamOn }) {
   const videoRef = useRef(null);
 
   useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        setStream(stream);
-        videoRef.current.srcObject = stream;
-      })
-      .catch((error) => console.error(error));
-  }, [setStream]);
+    if (streamOn) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true, audio: true })
+        .then((stream) => {
+          setStream(stream);
+          videoRef.current.srcObject = stream;
+        })
+        .catch((error) => console.error(error));
+    } else {
+      if (videoRef.current && videoRef.current.srcObject) {
+        const tracks = videoRef.current.srcObject.getTracks();
+        tracks.forEach((track) => track.stop());
+        videoRef.current.srcObject = null;
+        setStream(null);
+      }
+    }
+  }, [setStream, streamOn]);
 
   return <video ref={videoRef} autoPlay muted />;
-};
-
-
+}
 
--- frontend/pages/create/index.page.jsx
+++ frontend/pages/create/index.page.jsx
@@ -1,9 +1,28 @@
-export { Page }
+/** 
+  add Button start-stream and stop-stream
+*/
+import React, { useState, useEffect, useRef } from "react";
+import SetupStream from "./SetupStream";
 
-function Page() {
-	return (
-		<div>
-			<h1>index.page.jsx</h1>
-		</div>
-	)
+export function Page() {
+  const [stream, setStream] = useState(null);
+  const [peerId, setPeerId] = useState(null);
+  const [streamOn, setStreamOn] = useState(false);
+
+  const handleStartStream = () => {
+    setStreamOn(true);
+  };
+
+  const handleStopStream = () => {
+    setStreamOn(false);
+  };
+
+  return (
+    <>
+      <SetupStream setStream={setStream} streamOn={streamOn} />
+      <button onClick={handleStartStream}>Start Stream</button>
+      <button onClick={handleStopStream}>Stop Stream</button>
+    </>
+  );
 }
+
