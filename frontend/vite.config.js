eb94c4892a0e8aa12d17e9df21249937833feb7a
Haiz14 <haiz.gigs@gmail.com>
2023-05-13 17:59:39 +0530

    UPDATED: Basename for vercel deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import react from '@vitejs/plugin-react'
 import ssr from 'vite-plugin-ssr/plugin'
 
 export default {
-  base: "/watch-it",
+  base: "/",
   plugins: [react(), ssr({prerender: true})]
 }
