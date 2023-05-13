39e99a543154c2b6cede280a5303d5830827c13b
Haiz14 <haiz.gigs@gmail.com>
2023-05-13 17:16:10 +0530

    UPDATED: Usage insrruction on index page

--- frontend/pages/index/index.page.jsx
+++ frontend/pages/index/index.page.jsx
@@ -4,16 +4,11 @@ import { Counter } from './Counter'
 export { Page }
 
 function Page() {
+  let content = `Click on create link in sidebar to create a new camera, and share the link generated with others who want to view your camera remotely.`
   return (
     <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
+      <h1>Usage</h1>
+      <p>{content}</p>
     </>
   )
 }
