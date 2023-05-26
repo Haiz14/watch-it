12d8240892fe7a0f0e6d3f843b53b8254f841c69
Haiz14 <haiz.gigs@gmail.com>
2023-05-26 14:19:34 +0530

    UPDATED: Added usage content, and delted the counter

--- frontend/pages/index/index.page.jsx
+++ frontend/pages/index/index.page.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 import { Counter } from './Counter'
 
 export { Page }
+// Use markdown
 
 function Page() {
+  let usageContent = `Indeed life is sad, Visit Create page; Create a camera. Visit the link generated to view your camera`
   return (
     <>
-      <h1>Welcome</h1>
-      This page is:
-      <ul>
-        <li>Rendered to HTML.</li>
-        <li>
-          Interactive. <Counter />
-        </li>
-      </ul>
+      <h1>Usage</h1>
+      <p>{usageContent}</p>
     </>
   )
 }
